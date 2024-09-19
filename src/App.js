a74a600c55bbe8c19d791f3d2b777fc0c707923c
Jacob Lee <117792381+JakeLee13@users.noreply.github.com>
2024-09-18 21:27:58 -0600

    Moved package.json and related files back to root

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';  // Import Header component
+import './App.css';  // General styling for the app
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />  {/* Displays the Header */}
+      <main>
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';  // Ensure you create the Header-specific styles here
+
+function Header() {
+  return (
+    <header>
+      <h1>Jacob Lee.<br /></h1>
+      <h2>Salt Lake City, Utah.</h2>
+    </header>
+  );
+}
+
+export default Header;
+
